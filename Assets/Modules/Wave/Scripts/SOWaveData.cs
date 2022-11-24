10fd4814c17c88e3dfb164388bb5aed7dd286984
Leandro Peres <24868191+zschzen@users.noreply.github.com>
2022-11-23 22:41:11 -0300

    CREATED: Initial wave files

---
+++ Assets/Modules/Wave/Scripts/SOWaveData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Modules.Wave
+{
+    [CreateAssetMenu(fileName = "Wave Data", menuName = "Wave/Data", order = 0)]
+    public class SOWaveData : ScriptableObject
+    {
+        [field: SerializeField] public int WaveNumber { get; private set; }
+        [field: SerializeField] public int EnemyCount { get; private set; }
+        [field: SerializeField] public float SpawnRate { get; private set; }
+        [field: SerializeField] public float SpawnDelay { get; private set; }
+    }
+}
---
+++ Assets/Modules/Wave/Scripts/WaveManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Modules.Wave
+{
+    public class WaveManager : MonoBehaviour
+    {
+        // Fields ----------------------------------------------------------------
+        [SerializeField] private SOWaveData m_waveData;
+        [SerializeField] private Transform[] m_spawnPoints;
+        
+        // Private Methods ------------------------------------------------------
+ 
+        
+    }
+}
