0e78f393417c9b5f87f76e8ed65682d62b9c2a93
Leandro Peres <24868191+zschzen@users.noreply.github.com>
2022-11-23 22:40:53 -0300

    ADDED: Enum missing files

---
+++ Assets/Modules/Enums/DiscardTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Enums
+{
+    [Flags]
+    public enum DiscardTypes
+    {
+        None = 0,
+        Organic = 1,
+        Metalic = 2,
+        Plastic = 4,
+
+
+        // Recyclable is metalic or plastic, but not organic
+        Recyclable = Metalic | Plastic,
+
+        // NonRecyclable is organic and not recyclable 
+        NonRecyclable = Organic | ~Recyclable
+    }
+}
