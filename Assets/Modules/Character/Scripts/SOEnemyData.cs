653aee0922fd8fee8dd6ac6c84a4fa79426f968c
Leandro Peres <24868191+zschzen@users.noreply.github.com>
2022-11-24 14:44:04 -0300

    CREATED: SOEnemyData

---
+++ Assets/Modules/Character/Scripts/SOEnemyData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Modules.Character
+{
+    [CreateAssetMenu(fileName = "Enemy Data", menuName = "Gameplay/Enemy Data", order = 1)]
+    public class SOEnemyData : SOCharacterData
+    {
+        [field: SerializeField] public float AttackRange { get; private set; }
+    }
+}
