4690995897be05742d8d31f4a8e0a4b2b02976be
Leandro Peres <24868191+zschzen@users.noreply.github.com>
2022-11-24 20:13:36 -0300

    FIX: General projectile fixes & refactoration to impl pooling system

--- Assets/Modules/Projectile/Scripts/Projectile.cs
+++ Assets/Modules/Projectile/Scripts/Projectile.cs
@@ -1,50 +1,67 @@
+using System;
 using DG.Tweening;
+using Enums;
 using UnityEngine;
+using UnityEngine.Pool;
 
 namespace Modules.Projectile
 {
     [RequireComponent(typeof(Collider), typeof(Rigidbody))]
     public class Projectile : MonoBehaviour
     {
-        [field: SerializeField] public SOProjectileData ProjectileData { get; private set; }
+        public event Action OnRelease;
+
+        [field: SerializeField] public DiscardTypes Type { get; private set; }
+        [field: SerializeField] public float Speed { get; private set; }
+        [field: SerializeField] public int Damage { get; private set; }
+        [field: SerializeField] public float LifeTime { get; private set; }
 
         [SerializeField] private Rigidbody m_rigidbody;
+        [SerializeField] private Renderer m_renderer;
+
+        // Public methods -----------------------------------------------------------------------------------------------
+
+        public void Launch(Vector3 direction)
+        {
+            m_rigidbody.velocity = direction * Speed;
+        }
 
         // Unity Methods ----------------------------------------------------------------------
 
         private void OnEnable()
         {
+            // Animate the projectile
+            transform.localScale = Vector3.zero;
+            _ = transform.DOScale(1f, 0.1f).SetEase(Ease.OutBack);
         }
 
         private void OnDisable()
         {
-            OnLifeTimeEnd();
+            _ = DOTween.Kill(this);
+
+            // Release the projectile back to the pool
+            OnRelease?.Invoke();
         }
 
-        private void Start()
-        {
-            m_rigidbody = GetComponent<Rigidbody>();
-            m_rigidbody.velocity = transform.forward * ProjectileData.Speed;
-
-            // Destroy the projectile after a certain amount of time
-            DOVirtual.DelayedCall(ProjectileData.LifeTime, OnLifeTimeEnd).SetId(this);
-        }
+        private void OnBecameInvisible() => gameObject.SetActive(false);
 
         private void OnTriggerEnter(Collider other)
         {
             if (!other.gameObject.CompareTag("Enemy")) return;
+            if (!other.TryGetComponent(out Enemy.Enemy enemy)) return;
 
-            other.gameObject.GetComponent<Enemy.Enemy>().TakeDamage(ProjectileData.Damage, ProjectileData.Type);
-            Destroy(gameObject);
+            enemy.TakeDamage(Damage, Type);
+
+            // Animate the projectile
+            _ = transform.DOScale(0f, 0.1f)
+                .OnComplete(() => gameObject.SetActive(false))
+                .SetEase(Ease.InBack);
 
             // TODO: Add particle effect
         }
 
         // Private Methods ---------------------------------------------------------------------
-        private void OnLifeTimeEnd()
-        {
-            _ = DOTween.Kill(this);
-            // TODO: Return to pool
-        }
+
+        private void SetColor(Color color) => m_renderer.material.color = color;
     }
 }
