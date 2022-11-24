57dc619fc12b915c5b5132fcd6ae762a8b97dd5e
Leandro Peres <24868191+zschzen@users.noreply.github.com>
2022-11-24 14:41:53 -0300

    CREATED: Wave module

---
+++ Assets/Modules/Wave/Scripts/WaveManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Modules.Wave
+{
+    public class WaveManager : MonoBehaviour
+    {
+        // Fields ----------------------------------------------------------------
+        [SerializeField] private SOWaveData m_waveData;
+        [SerializeField] private Transform[] m_spawnPoints;
+        
+        private int m_currentWaveIndex;
+
+        // Unity Methods --------------------------------------------------------
+        
+        private void Start()
+        {
+            m_currentWaveIndex = 0;
+            SpawnWave();
+        }
+        
+        // Private Methods ------------------------------------------------------
+        
+        private Transform GetRandomSpawnPoint() => m_spawnPoints[Random.Range(0, m_spawnPoints.Length)];
+
+        private void SpawnWave()
+        {
+            if (m_currentWaveIndex >= m_waveData.TotalWaveCount)
+            {
+                Debug.Log("No more waves");
+                return;
+            }
+
+            for (int i = 0; i < m_waveData.EnemyCount; i++)
+            {
+                var spawnPoint = m_spawnPoints[Random.Range(0, m_spawnPoints.Length)];
+                //Instantiate(m_waveData.EnemyPrefab, spawnPoint.position, Quaternion.identity);
+            }
+        }
+
+    }
+}
