38ee470c6298414276c26d637810c558fd5ba1ab
leandrogonzaga <leandroperes@protonmail.com>
2022-11-25 15:34:45 -0300

    ADD: Basic Levels

---
+++ Assets/Modules/Levels/Scripts/SOLevel.cs
@@ -0,0 +1,13 @@
+﻿using Modules.Wave;
+using UnityEngine;
+
+namespace Modules.Levels.Scripts
+{
+    [CreateAssetMenu(fileName = "Level Data", menuName = "Gameplay/Level", order = 0)]
+    public class SOLevel : ScriptableObject
+    {
+        [field: SerializeField] public SOWaveData WaveData { get; private set; }
+        [field: SerializeField] public int Level { get; private set; }
+        [field: SerializeField] public int Waves { get; private set; }
+    }
+}
