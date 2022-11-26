4d775f5092d23d060fd13b948046c9993cd2d521
Leandro Peres <24868191+zschzen@users.noreply.github.com>
2022-11-26 15:34:46 -0300

    Intro logic

--- Assets/Modules/GameManager/Scripts/GameManager.cs
+++ Assets/Modules/GameManager/Scripts/GameManager.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections;
+using System.Reflection;
+using DG.DOTweenEditor;
+using DG.Tweening;
 using Modules.Collectable;
+using Modules.Dialogue;
 using Modules.Wave;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -14,17 +18,33 @@ namespace Modules.GameManager
         [SerializeField] private WaveManager m_waveManager;
         [SerializeField] private CollectableFactory m_collectableFactory;
 
-        // Start coroutine
-        private IEnumerator Start()
-        {
-            yield return new WaitForSeconds(m_gameDelay);
+        [SerializeField] private Dialogue.Dialogue m_introDialogue, m_gameoverDialogue, m_winDialogue;
+        [SerializeField] private float m_introDialogueDelay = .5f;
 
+        private void Start()
+        {
+            DOVirtual.DelayedCall(m_introDialogueDelay, () => m_introDialogue.Show(true));
+
+            m_introDialogue.OnDialogueEnd.AddListener(StartGame);
+        }
+
+        // Start coroutine
+        private void StartGame()
+        {
+            StartCoroutine(GameStartRoutine());
+        }
+
+        private IEnumerator GameStartRoutine()
+        {
             // Spawn random collectable
-            var amount = Random.Range(5, 10);
+            var amount = Random.Range(10, 20);
 
             for (var i = 0; i < amount; i++)
             {
-                var collectable = m_collectableFactory.CreateRandomCollectable();
+                // set a new seed
+                Random.InitState((int)Time.time + i);
+
+                var collectable = m_collectableFactory.CreateRandomTypeCollectable();
                 collectable.transform.position = new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5));
             }
             
